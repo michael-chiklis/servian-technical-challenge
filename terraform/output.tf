0dea804a358448e2a458ab1648ab7811fe2958fe
Michael Chiklis <michael.chiklis@fastmail.com>
2021-05-02 22:48:41 +1000

    Remove DB password output

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,9 +1,3 @@
-# TODO remove when finished
-output "db_password" {
-  value     = random_password.db_password.result
-  sensitive = true
-}
-
 output "alb_domain" {
   value = aws_lb.alb.dns_name
 }
